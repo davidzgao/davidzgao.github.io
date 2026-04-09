95ea1b105751a41009da5cffed37d8d60cc7b65d
davidzgao <gaozhehere@yeah.net>
2026-04-09 20:10:33 +0800

    test

--- js/backtop.js
+++ js/backtop.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
 
     // fade in #back-top
     $(function() {
-    //    $(window).scroll(function() {
-    //        if ($(this).scrollTop() > 100) {
-    //            $('#back-top').fadeIn();
-    //        } else {
-    //            $('#back-top').fadeOut();
-     //       }
-    //    });
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+           }
+        });
 
         // scroll body to 0px on click
-     //   $('#back-top a').click(function() {
-     //       $('body,html').animate({
-     //           scrollTop: 0
-     //       }, 800);
-     //       return false;
-     //   });
+       $('#back-top a').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
     });
 
 });
