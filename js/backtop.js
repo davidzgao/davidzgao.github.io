c5d2a813b7ae227a4de40d14a7dfd32993b61cc0
davidzgao <gaozhehere@yeah.net>
2026-04-09 19:51:04 +0800

    test

--- js/backtop.js
+++ js/backtop.js
@@ -14,12 +14,12 @@ $(document).ready(function() {
     //    });
 
         // scroll body to 0px on click
-        $('#back-top a').click(function() {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
+     //   $('#back-top a').click(function() {
+     //       $('body,html').animate({
+     //           scrollTop: 0
+     //       }, 800);
+     //       return false;
+     //   });
     });
 
 });
